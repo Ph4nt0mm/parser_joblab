d8a3602f1dad08616c6896a758741320f9452523
Fedor Tropin <tropinfyodor@it.adcombo.com>
2024-04-16 16:57:47 +0300

    feat:  extend send_request doc, pass data to post

--- src/support_modules/api_menager.py
+++ src/support_modules/api_menager.py
@@ -10,22 +10,23 @@ class APIManager:
         self.base_url = base_url
         self.session = Session()
 
-    def send_request(self, endpoint: str, params: dict = None, method: str = 'GET') -> Response:
+    def send_request(self, endpoint: str, params: dict = None, method: str = 'GET', data: dict = None) -> Response:
         """
         Sends a request to the specified API endpoint.
 
         Args:
             endpoint (str): The endpoint path to append to the base URL.
-            params (dict): Parameters to include in the request.
-            method (str): HTTP method to use.
+            params (dict, optional): Parameters to include in the request as a query string.
+            method (str, optional): HTTP method to use (default is 'GET').
+            data (dict, optional): Data to send in the body of the request (useful for 'POST' requests).
 
         Returns:
-            Response: The response from the API server.
+            Response: The response object from the API server.
         """
         url = f"{self.base_url}{endpoint}"
         if method.upper() == 'GET':
             return self.session.get(url, params=params)
         elif method.upper() == 'POST':
-            return self.session.post(url, data=params)
+            return self.session.post(url, params=params, data=data)
         else:
             raise ValueError('Unsupported HTTP method')
